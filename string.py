a8cc5c02c7916cac62ac9e490dc5109f64a9bde0
Shaheer25 <shaheer@logichive.in>
2023-08-24 18:09:00 +0530

    code

---
+++ string.py
@@ -0,0 +1,9 @@
+original_string = "JavaScript"
+new_string = original_string[1::2]  # Start from index 1, take every second character
+
+print("Original string:", original_string)
+print("New string:", new_string)
+
+# OP:
+# Original string: JavaScript
+# New string: aacit
