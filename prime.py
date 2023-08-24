a5d4168940099d07285f897ad6a56e6fbf66defa
Shaheer25 <shaheer@logichive.in>
2023-08-24 11:40:50 +0530

    code

---
+++ prime.py
@@ -0,0 +1,14 @@
+def isprime(num):
+    if num == 0 :
+        return False
+    for i in range (2, int(num **0.5)+1):
+        if num % i == 0 :
+            return False
+    return True
+
+print("The prime Numbers are")
+for i in range(100):
+    if isprime(i):
+        print(i, end = "\n")
+
+print()
